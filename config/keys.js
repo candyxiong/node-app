78caccf4b66545c8b220cf3f5ef87df7197eec48
xhy <695487524@qq.com>
2018-10-10 15:07:57 +0800

    build server

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURL:"mongodb://test:x123456@ds261450.mlab.com:61450/restful-api-prod"
+}
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+//引入users.js
+const users = require('./routes/api/users');
+
+//DB config
+const db = require('./config/keys').mongoURL;
+
+//Connect to mongodb
+mongoose.connect(db)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err))
+
+
+app.get('/',(req,res) => {
+    res.send('hello world!')
+})
+
+//使用routes
+app.use('/api/users',users);
+
+const port = process.env.PORT || 5000;
+app.listen(port,()=>{
+   console.log(`hello ${port}`)
+})
